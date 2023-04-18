a2de74db0d71322d84fc6f88c2bf0f85a057f62d
Dmrchk <105112928+Dmrchk@users.noreply.github.com>
2023-04-18 14:01:14 +0200

    Add files via upload

---
+++ note/src/main/java/com/od/dev/SpringNoteProjectApplication.java
@@ -0,0 +1,12 @@
+package com.od.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNoteProjectApplication.class, args);
+    }
+}
